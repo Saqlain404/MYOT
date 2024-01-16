d6671edc2f23015d95f0fb5bf8efc1ada93cd37b
PramodK2807 <pramodkushwaha1328@gmail.com>
2024-01-16 19:03:31 +0530

    fix: edit profile

--- src/Components/Index/EditProfile.jsx
+++ src/Components/Index/EditProfile.jsx
@@ -32,11 +32,7 @@ const EditProfile = () => {
   } = useForm({
     mode: "onChange",
   });
-
-  const getEmployeeList = async () => {
-    let { data } = await EmployeeLists();
-  };
-
+  
   const onFileSelection = (e, key) => {
     // console.log(e.target.files, key);
     setFiles({ ...files, [key]: e.target.files[0] });
@@ -47,7 +43,7 @@ const EditProfile = () => {
 
   const onSubmit = async (data1) => {
     console.log(files?.profile_img);
-    // let emp_id = localStorage.getItem("myot_admin_id");
+    let emp_id = localStorage.getItem("myot_admin_id");
     if (data1?.password !== data1?.cpassword) {
       Swal.fire({
         toast: true,
@@ -73,7 +69,7 @@ const EditProfile = () => {
     formData.append("address", data1?.companyAddress);
     formData.append("companyName", data1?.companyName);
 
-    let { data } = await UpdateAdminProfile( formData);
+    let { data } = await UpdateAdminProfile(emp_id, formData);
     console.log(data);
     if (data && data?.error) {
       Swal.fire({
@@ -98,10 +94,9 @@ const EditProfile = () => {
         timer: 3000,
       });
       setFiles([]);
-      // dispatch(updateProfilePic(data?.results?.?.profile_Pic));
-      // dispatch(updateUserName(data?.results?.?.name));
-      navigate("/Department/My-profile");
-      getEmployeeList();
+      dispatch(updateProfilePic(data?.results?.admin?.profile_Pic));
+      dispatch(updateUserName(data?.results?.admin?.name));
+      navigate("/Admin/My-profile");
     }
   };
   return (
